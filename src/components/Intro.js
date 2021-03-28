ef04c7a22675d78dc756499d4f02cacb9fc8c9d7
Chris Calhoun <[ctcalhoun2@gmail.com]>
2021-03-28 14:08:20 -0500

    created temporary footer content.

---
+++ src/components/Intro.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Intro() {
+  return (
+    <>
+      <h3>Hi, I'm Chris</h3>
+      <p>
+        I am a part-time student at Nashville Software School (NSS) pursuing a
+        career in software engineering. It has been a long winding road to get
+        to this point, but I am incredibly thankful and excited to be here.
+      </p>
+      <p>
+        After graduating with a degree in mechanical engineering, I worked in a
+        variety of technical roles, including analyzing paint quality data,
+        designing concert stages, and site planning hospital equipment. While
+        each of these roles were very different, they all presented
+        opportunities to automate and code. To make a long story short, I
+        quickly fell in love with coding and now aspire to make a long term
+        career out of it.
+      </p>
+      <p>
+        Outside of school work and work work, I enjoy climbing, chess, and ultimate frisbee(!). Thanks for reading and feel free to contact me here!
+      </p>
+    </>
+  );
+}
+
+export default Intro;
---
+++ src/components/ProfilePic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProfilePic() {
+  return (
+    <>
+      <h1>Picture</h1>
+    </>
+  );
+}
+
+export default ProfilePic;
