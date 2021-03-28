0f0ce33e3b720e82afd5fec07ddc8d75fe957bef
Chris Calhoun <[ctcalhoun2@gmail.com]>
2021-03-28 13:33:52 -0500

    completed file structure and added component files.

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <>
+      <nav className='navbar'>
+        <Link to='/' className='navbar-header'>
+          CHRIS CALHOUN
+        </Link>
+        <Link to='/' className='navbar-header'>
+          experience
+        </Link>
+        <Link to='/' className='navbar-header'>
+          projects
+        </Link>
+        <Link to='/' className='navbar-header'>
+          contact
+        </Link>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
